358f56178e74320b41737c4228e027863cfe8567
Druserdo <Druserdo@gmail.com>
2024-01-18 22:06:40 +0200

    les

--- Lesson6/Program.cs
+++ Lesson6/Program.cs
@@ -1,9 +1,21 @@
-﻿int c = (int)new Random().NextDouble();
-Console.WriteLine(c);
+﻿//int c = (int)new Random().NextDouble();
+//Console.WriteLine(c);
 
-//5
-string myPerson = "Viktor\nOduew";
-Console.WriteLine(myPerson);
-//6
-string myString = "284";
-Console.WriteLine(int.Parse(myString));
+////5
+//string myPerson = "Viktor\nOduew";
+//Console.WriteLine(myPerson);
+////6
+//string myString = "284";
+//Console.WriteLine(int.Parse(myString));
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+int a = number/1000;
+Console.WriteLine(a);
+int b = number % 1000 / 100;
+Console.WriteLine(b);
+int c = number % 100 / 10;
+Console.WriteLine(c);
+int d = number % 10;
+Console.WriteLine(d);
+Console.WriteLine($"сумма цифр: {a + b + c + d}");
+Console.WriteLine($"Произведение цифр: {a * b * c * d}");
--- Lesson6/obj/Debug/net8.0/Lesson6.AssemblyInfo.cs
+++ Lesson6/obj/Debug/net8.0/Lesson6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lesson6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0547466f90e8fde1963e539953d24841215b807")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lesson6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lesson6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
